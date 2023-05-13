e4002ffe31e25c1be84d6914f6142dc0c8f1b935
jongwoo Yoo <dbwhddn10@gmail.com>
2023-05-13 14:54:32 +0900

    change angular httpClient to axios instance

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,88 +1,123 @@
-import {
-  HttpBackend,
-  HttpClient,
-  HttpHandler,
-  HttpXhrBackend,
-  HTTP_INTERCEPTORS,
-  ɵHttpInterceptingHandler,
-} from '@angular/common/http';
-import { Injectable, Injector } from '@angular/core';
-import { AuthTokenApiInterceptor } from 'src/app/provider/interceptor/api/auth-token.provider';
-import { BaseUrlApiInterceptor } from 'src/app/provider/interceptor/api/base-url.provider';
-import { JsonifyApiInterceptor } from 'src/app/provider/interceptor/api/jsonify.provider';
-import { ModelifyApiInterceptor } from 'src/app/provider/interceptor/api/modelify.provider';
+import * as _ from 'lodash';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { Model } from 'src/app/model';
+import { Observable, Observer } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const requestAuthTokenInterceptor = (req: any) => {
+  if (localStorage.getItem('ichoodate-auth-token')) {
+    req.headers['Authorization'] =
+      'Bearer ' + localStorage.getItem('ichoodate-auth-token');
+  }
+  return req;
+};
+
+const requestBaseUrlInterceptor = (req: any) => {
+  req.url = `${environment.apiUrl}/` + req.url;
+  return req;
+};
+const requestParamInterceptor = (req: any) => {
+  if (req.params) {
+    _.forEach(req.params, (value, key) => {
+      if (_.isArray(value)) {
+        req.params[key] = value.toString();
+      }
+    });
+  }
+  return req;
+};
+
+const responseJsonParserInterceptor = (res: any) => {
+  try {
+    res.data = JSON.parse(res.data);
+  } catch (e) {}
+  return res;
+};
+
+const responseModelifyInterceptor = (res: any) => {
+  const objectify = (data: any) => {
+    let isModel = Array.isArray(data) ? false : true;
+    let collect = isModel ? [data] : data;
+    let models = _.map(collect, (item: any) => {
+      let type = item._type;
+      let attributes = item._attributes;
+      let relations = item._relations;
+      let modelClass = _.upperFirst(_.camelCase(type.split('/').pop()));
+      let moduleLoader = require('src/app/model/' + type.replace(/_/g, '-'));
+      let model = <Model<any, any>>new moduleLoader[modelClass]();
+
+      _.each(attributes, (value, key) => {
+        model.setAttr(key, value);
+      });
+
+      _.each(relations, (value: any, key: any) => {
+        model.setRelation(key, value == null ? value : objectify(value));
+      });
+
+      return model;
+    });
+
+    return isModel ? models[0] : models;
+  };
+
+  const body = res.data;
+
+  if (typeof body == 'undefined') {
+    return res;
+  }
+
+  if (typeof body.errors != 'undefined') {
+    for (const error in body.errors) {
+      alert(JSON.stringify(body.errors[error]));
+    }
+    throw new Error(JSON.stringify(body.errors));
+  }
+  if (typeof body.result != 'object' || !body.result) {
+  } else if (typeof body.result.data != 'undefined') {
+    body.result.data = objectify(body.result.data);
+  } else {
+    body.result = objectify(body.result);
+  }
+
+  res.data = body.result;
+  return res;
+};
+
+const setUpHttpProxy = (httpClient: any) => {
+  ['get', 'post', 'put', 'patch', 'delete', 'options'].forEach((method) => {
+    httpClient[method] = new Proxy(httpClient[method], {
+      apply: <T>(target: any, thisArgs: any, argArray: any[]) => {
+        return new Observable<T>((observer: Observer<T>) => {
+          target(...argArray)
+            .then((response: AxiosResponse) => {
+              observer.next(response.data);
+            })
+            .catch((error: AxiosError | Error) => {
+              observer.error(error);
+            })
+            .finally(() => {
+              observer.complete();
+            });
+        });
+      },
+    });
+  });
+  return httpClient;
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
-  static apiClient: HttpClient;
-
-  private static init(client: string, injector: Injector): HttpClient {
-    if (
-      Object.getOwnPropertyDescriptor(HttpService, client)?.value === undefined
-    ) {
-      Object.defineProperty(HttpService, client, {
-        value: Injector.create({
-          providers: [
-            {
-              provide: HttpBackend,
-              useValue: new HttpXhrBackend({
-                build: () => new XMLHttpRequest(),
-              }),
-            },
-            {
-              provide: Injector,
-              useValue: injector,
-            },
-            {
-              provide: HttpHandler,
-              useClass: ɵHttpInterceptingHandler,
-              deps: [HttpBackend, Injector],
-            },
-            {
-              provide: HttpClient,
-              deps: [HttpHandler],
-            },
-          ],
-        }).get(HttpClient),
-      });
-    }
-
-    return Object.getOwnPropertyDescriptor(HttpService, client)?.value;
-  }
-
-  public static api(): HttpClient {
-    return HttpService.init(
-      'apiClient',
-      Injector.create({
-        providers: [
-          {
-            provide: HTTP_INTERCEPTORS,
-            useClass: AuthTokenApiInterceptor,
-            multi: true,
-            deps: [],
-          },
-          {
-            provide: HTTP_INTERCEPTORS,
-            useClass: BaseUrlApiInterceptor,
-            multi: true,
-            deps: [],
-          },
-          {
-            provide: HTTP_INTERCEPTORS,
-            useClass: JsonifyApiInterceptor,
-            multi: true,
-            deps: [],
-          },
-          {
-            provide: HTTP_INTERCEPTORS,
-            useClass: ModelifyApiInterceptor,
-            multi: true,
-            deps: [],
-          },
-        ],
-      })
-    );
+  static api(): HttpClient {
+    const httpClient = axios.create();
+    httpClient.interceptors.request.use(requestAuthTokenInterceptor);
+    httpClient.interceptors.request.use(requestBaseUrlInterceptor);
+    httpClient.interceptors.request.use(requestParamInterceptor);
+    httpClient.interceptors.response.use(responseJsonParserInterceptor);
+    httpClient.interceptors.response.use(responseModelifyInterceptor);
+    return setUpHttpProxy(httpClient);
   }
 }
